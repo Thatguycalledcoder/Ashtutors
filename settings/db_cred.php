fa4ca2aab0f1c0da4f0dbef5aa2baa835282368d
Thatguycalledcoder <quarshiedavid62@gmail.com>
2022-12-07 13:24:14 +0000

    db

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,13 +1,13 @@
 <?php
 //Database credentials
-define("SERVERNAME", "localhost");
-define("HOSTNAME", "root");
-define("PASSWD", "");
-define("DATABASE", "ashtutor");
+// define("SERVERNAME", "localhost");
+// define("HOSTNAME", "root");
+// define("PASSWD", "");
+// define("DATABASE", "ashtutor");
 
-// define("SERVERNAME", "us-cdbr-east-06.cleardb.net");
-// define("HOSTNAME", "b6033d33c24584");
-// define("PASSWD", "62d6bf1d");
-// define("DATABASE", "heroku_ca93362035ee3ac");
+define("SERVERNAME", "us-cdbr-east-06.cleardb.net");
+define("HOSTNAME", "b6033d33c24584");
+define("PASSWD", "62d6bf1d");
+define("DATABASE", "heroku_ca93362035ee3ac");
 
 ?>
